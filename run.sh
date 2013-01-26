f5c71cc2680cdccdc78770a67cc184a840145b5a
Aditya Vaidya <kroq.gar78@gmail.com>
2013-01-25 23:54:36 -0600

    run.sh: added extraction functionality

--- run.sh
+++ run.sh
@@ -6,3 +6,11 @@ sponsor_email=$3
 school_addr=$4
 
 # do some stuff
+
+# dump fields into YAML file
+pdftk isef_1a.pdf dump_data_fields | pdffdf -ln > isef_1a_fields.yml
+
+# copy PDF to separate file for safe keeping
+#cp -av "$1.pdf"
+
+# dump the new fields back into the PDF
