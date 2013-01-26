128958524c63f3aa26849870a41c234fc9c1ad92
Aditya Vaidya <kroq.gar78@gmail.com>
2013-01-26 00:13:47 -0600

    Updated 'dump-fields', added a quick (untested) export for the final PDF

--- dump-fields.sh
+++ dump-fields.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-pdftk isef_1a.pdf dump_data_fields > isef_1a_fields.txt
+###############
+### WARNING ###
+###############
+
+# DO NOT RUN IF THE YML IS ALREADY GENERATED!!!!!
+
+pdftk isef_1a.pdf dump_data_fields | pdffdf -ln > isef_1a_fields.yml
--- run.sh
+++ run.sh
@@ -8,10 +8,12 @@ school_addr=$4
 # do some stuff
 
 # dump fields into YAML file
-pdftk isef_1a.pdf dump_data_fields | pdffdf -ln > isef_1a_fields.yml
+#pdftk isef_1a.pdf dump_data_fields | pdffdf -ln > isef_1a_fields.yml
 
 # copy PDF to separate file for safe keeping
 #cp -av "$1.pdf"
 
-# edit the YAML file with the new parameters
+# TODO: edit the YAML file with the new parameters
 
+# export the final PDF
+pdftk isef_1a.pdf fill_form "$name.fdf" output "$name.pdf"
