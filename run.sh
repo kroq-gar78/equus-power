68ff774e2bd579214af1ba644e570793b8f0601a
Aditya Vaidya <kroq.gar78@gmail.com>
2013-01-26 00:23:02 -0600

    lulzy

--- run.sh
+++ run.sh
@@ -5,15 +5,13 @@ sponsor_name=$2
 sponsor_email=$3
 school_addr=$4
 
-# do some stuff
-
 # dump fields into YAML file
 #pdftk isef_1a.pdf dump_data_fields | pdffdf -ln > isef_1a_fields.yml
 
 # copy PDF to separate file for safe keeping
-#cp -av "$1.pdf"
+#cp -av "$name.pdf"
 
-# TODO: edit the YAML file with the new parameters
+# TODO: create the YAML file with the new parameters
 
 # merge the master and student YAML files and convert to FDF
 pdffdf -g isef_1a_fields.yaml "$name.yml" > "$name.fdf"
